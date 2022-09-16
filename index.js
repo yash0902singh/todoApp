1f2ce7dc570ea45fcf138f53bd83da6df3522e6e
yash0902singh <73191510+yash0902singh@users.noreply.github.com>
2022-09-16 12:39:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require("body-parser");
+const app = express();
+const con = require('./models/taskModel')
+
+app.use(express.static('public'));
+app.engine('hbs', hbs({
+    helpers: {
+        isCompleted: function (status) {
+            if (status == "completed") {
+                return true
+            } else {
+                return false
+            }
+        },
+    },
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM Todo";
+    let items = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        items = result
+        console.log(items)
+        res.render('index', {
+            items: items
+        })
+    })
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    let query = "INSERT INTO Todo (task, status) VALUES ?";
+    data = [
+        [req.body.task, "ongoing"]
+    ]
+    con.query(query, [data], (err, result) => {
+        if (err) throw err;
+        console.log(result)
+        res.redirect('/')
+    })
+})
+
+app.listen(3000, () => {
+    console.log('The web server has started on port 3000');
+});
